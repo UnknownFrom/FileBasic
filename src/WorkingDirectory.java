c9b96fe99f1aea30ac7ea0595a52fdd094883643
UnknownFrom <87531160+UnknownFrom@users.noreply.github.com>
2021-09-24 15:07:10 +0300

    Первый коммит

---
+++ src/WorkingDirectory.java
@@ -0,0 +1,143 @@
+import java.io.File;
+import java.io.FileFilter;
+
+public class WorkingDirectory {
+    private static WorkingDirectory instance;
+    private String directoryName;
+
+    private WorkingDirectory(String directoryName) {
+        this.directoryName = directoryName;
+    }
+
+    public static WorkingDirectory getInstance(String directoryName) {
+        if (instance == null) {
+            instance = new WorkingDirectory(directoryName);
+        }
+        return instance;
+    }
+
+    public void ToPrint()
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i<s.length;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+    public void ToPrintPath()
+    {
+        System.out.println(directoryName);
+    }
+
+    public void ToParentName()
+    {
+        String s = new File(directoryName).getParent();
+        System.out.println(s.substring(s.lastIndexOf("\\")));
+    }
+
+    public void ToParent()
+    {
+        directoryName = new File(directoryName).getParent();
+    }
+
+    public boolean checkChildDir(String childName)
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i < s.length; i++)
+        {
+            if (new File(directoryName + "\\" + s[i]).isDirectory() && s[i].equals(childName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void ToCreateSubdir(String name)
+    {
+        File file = new File(directoryName + "\\" + name);
+        file.mkdir();
+    }
+
+    public void ToChildDir(String name)
+    {
+        File file = new File(directoryName + "\\" + name);
+        if(file.isDirectory()) {
+            directoryName = directoryName + "\\" + name;
+        }
+    }
+
+    public void DeleteAllDir()
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i < s.length; i++)
+        {
+            if (new File(directoryName + "\\" + s[i]).isDirectory()) {
+                directoryName = directoryName + "\\" + s[i];
+                DeleteAllDir();
+            }
+            File nFile = new File(directoryName + "\\" + s[i]);
+            nFile.delete();
+        }
+        directoryName = file.getParent();
+    }
+
+    public void ToFindEx(String ex)
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i< s.length; i++)
+        {
+            if(s[i].endsWith(ex))
+            {
+                System.out.println(s[i]);
+            }
+        }
+    }
+
+    public void ToPrintSubdir(String out)
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i < s.length; i++)
+        {
+            if (new File(directoryName + "\\" + s[i]).isDirectory()) {
+                System.out.println(out+s[i]);
+                directoryName = directoryName + "\\" + s[i];
+                ToPrintSubdir(out + "  ");
+            }
+            else
+            {
+                System.out.println(out+s[i]);
+            }
+        }
+        directoryName = file.getParent();
+    }
+
+    public void checkFindSubdir(String name)
+    {
+        File file = new File(directoryName);
+        String[] s = file.list();
+        for (int i = 0; i < s.length; i++)
+        {
+            if (new File(directoryName + "\\" + s[i]).isDirectory())
+            {
+                if (s[i].equals(name))
+                {
+                    System.out.println("Существует: " + directoryName + "\\" + s[i]);
+                }
+                else
+                {
+                    directoryName = directoryName + "\\" + s[i];
+                    checkFindSubdir(name);
+                }
+            }
+        }
+        directoryName = file.getParent();
+    }
+
+
+}
